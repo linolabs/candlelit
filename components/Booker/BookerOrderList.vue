0fb1a3cfe8addfc4102e7eb4473920a325b5a534
AsyncFox <59872475+AsyncFox@users.noreply.github.com>
2024-05-04 13:06:03 +0000

    fix: require login to add order

--- components/Booker/BookerOrderList.vue
+++ components/Booker/BookerOrderList.vue
@@ -7,7 +7,7 @@
       <Button
         class="border border-primary shadow-md w-10 h-10 sm:w-12 sm:h-12 p-2"
         variant="outline"
-        @click="() => useOrderEditDialog()"
+        @click="handleClick"
       >
         <Icon icon="ph:first-aid-duotone" class="w-8 h-8 sm:w-10 sm:h-10 text-orange-500" />
       </Button>
@@ -24,7 +24,16 @@
 
 <script setup lang="ts">
 import { Icon } from '@iconify/vue/dist/iconify.js';
+import { toast } from 'vue-sonner';
 import { useOrderEditDialog } from '@/composables/dialog';
 
 const bookerStore = useBookerStore();
+const userStore = useUserStore();
+function handleClick() {
+  if (!userStore.loggedIn) {
+    toast.error('请先登录');
+    return;
+  }
+  useOrderEditDialog();
+}
 </script>
